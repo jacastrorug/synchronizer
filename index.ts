2e3c0e0be885e806bd66ab367bdd0f553b7e54b4
jacastrorug <juliancastroruge@gmail.com>
2022-05-25 16:22:11 -0500

    Fix to update stock, price, stock available in wp_meta

--- index.ts
+++ index.ts
@@ -75,7 +75,13 @@ const synchronizeAccessories = async (accessories: AccessoryDNS[]) => {
 
             const [result] = await conexion.promise().query(`SELECT product_id, sku, stock_quantity, min_price, max_price FROM ${loadDBName}.wp_wc_product_meta_lookup WHERE sku = ?`, [accessory.codigoStock]);
             if (!result[0])
-                throw new Error(`Item not found in store database.`);
+                throw new Error(`Item not found in store database: ${accessory.codigoStock}.`);
+            
+            const [postMetaWP] = await conexion.promise().query(`SELECT * FROM ${loadDBName}.wp_postmeta WHERE meta_key = ? AND meta_value = ?`, ['_sku', accessory.codigoStock]);
+            if(!postMetaWP[0])
+                throw new Error(`Item not found in wp_postmeta: ${accessory.codigoStock}.`);
+            
+            console.log(`✅ Updating wp_wc_product_meta_lookup:`)
 
             const item: AccesoryStock = result[0];
             console.log(`Current stock: ${item.stock_quantity} -> new stock: ${accessory.stock}`);
@@ -83,6 +89,20 @@ const synchronizeAccessories = async (accessories: AccessoryDNS[]) => {
             console.log(`Current max price: ${item.max_price} -> new max price: ${accessory.valorConIva}`);
 
             await conexion.promise().query(`UPDATE ${loadDBName}.wp_wc_product_meta_lookup SET stock_quantity = ?, min_price = ?, max_price = ? WHERE sku = ?`, [accessory.stock, accessory.valorConIva, accessory.valorConIva, accessory.codigoStock]);
+
+            console.log(`✅ Updating wp_postmeta:`);
+            const postMeta: PostMetaWP = postMetaWP[0];
+
+            const [resultPostMetas] = await conexion.promise().query(`SELECT * FROM ${loadDBName}.wp_postmeta WHERE post_id = ? AND meta_key IN (?, ?, ?, ?, ?)`, [postMeta.post_id, '_sku', '_stock', '_regular_price', '_price', '_manage_stock']);
+            const postMetasWP = resultPostMetas as PostMetaWP[];
+            for(let idx = 0; idx < postMetasWP.length; idx++) {
+                console.log(`Meta key: ${postMetasWP[idx].meta_key} - Meta Value: ${postMetasWP[idx].meta_value}`);
+            }
+
+            await conexion.promise().query(`UPDATE ${loadDBName}.wp_postmeta SET meta_value = ? WHERE meta_key = ? AND post_id = ?`, [accessory.valorConIva, '_regular_price', postMeta.post_id]);
+            await conexion.promise().query(`UPDATE ${loadDBName}.wp_postmeta SET meta_value = ? WHERE meta_key = ? AND post_id = ?`, [accessory.valorConIva, '_price', postMeta.post_id]);
+            await conexion.promise().query(`UPDATE ${loadDBName}.wp_postmeta SET meta_value = ? WHERE meta_key = ? AND post_id = ?`, [accessory.stock, '_stock', postMeta.post_id]);
+            await conexion.promise().query(`UPDATE ${loadDBName}.wp_postmeta SET meta_value = ? WHERE meta_key = ? AND post_id = ?`, ['yes', '_manage_stock', postMeta.post_id]);
 
         } catch (error) {
             console.info(`The accessory: ${accessory.descripcion} - ${accessory.codigoStock} can't be updated: ${error}`);
---
+++ models/PostMetaWP.ts
@@ -0,0 +1,6 @@
+class PostMetaWP {
+    meta_id: number;
+    post_id: number;
+    meta_key: string;
+    meta_value: string;
+}
